37e2afb0ab1db80d87d8d961993e3727c029d7db
lEmanl <emanuelurbina@live.com>
2020-04-07 16:34:34 -0500

    Changed some formatting and added semaphores.

--- Threaded_CSMC.c
+++ Threaded_CSMC.c
@@ -5,105 +5,115 @@
 #include <pthread.h>
 #include <sys/time.h>
 #include <semaphore.h>
-#define MAX_STUDENTS 25
-int numOfStudents;
-int numOfTutors;
-int numOfChairs;
-int help;
-int MAX;
-sem_t waitingRoom;
+
+
+
+
+// STRUCTS
+struct StudentNode {
+    int threadId;
+    sem_t studentWaiting;
+};
+
+struct StudentQueue {
+    struct StudentNode * student;
+    struct StudentNode * nextStudent;
+};
+
+
+
+// GLOBAL VARIABLES
+sem_t mutexChairs = 1;
+sem_t mutexArrivedStudentQ = 1;
+sem_t mutexWaitingStudentQ = 1;
+
+sem_t coordinatorWaiting = 0;
+sem_t tutorWaiting = 0;
+
 int **studentIDQueue;
 int **priorities;
 int newStudentInQueue;
-/// comment1
-// comment 2
-void *notifyCoordinator();
+
+
+
+
+//  STUDENT THREAD
 void *student()
 {
-  printf("bye");
-  sem_wait(&waitingRoom);
-  printf("bye(1)");
-  //Queues student based on priority
-  notifyCoordinator();
-  sem_post(&waitingRoom);
-  //sem_post(&helpStudent);
-  //Notifies an idle tutor
 
 }
-/*
-void *tutor()
-{
-  while (finishedTutoring == 0)
-  {
-     sem_wait(helpStudent)
-     
-  }
-  sem_wait(&studentQueue);
-  sem_wait(&requiredNumberOfHelpTimes);
-}
-*/
-void *notifyCoordinator()
-{
-  newStudentInQueue = 1;
-}
+
+
+
+
+//  COORDINATOR THREAD
 void *coordinator()
 {
-  if (newStudentInQueue == 1)
-  {
-   //printf("Hello");
-    studentIDQueue[getLengthOfIDQueue()] = studentIDQueue[getLengthOfIDQueue() - 1] + 1;
-    //priorities[getLengthOfIDQueue()] = atoi(1);
-    printf("Student %d takes a seat", studentIDQueue[getLengthOfIDQueue()]);
-  }
-}
-int getLengthOfIDQueue()
-{
-   //Gets length of path variable
-    int index;
-    index = 0;
-    while (studentIDQueue[index] != NULL)
-    {
-       index++;
-    }
-    return index;
+
 }
 
+
+
+
+//  TUTOR THREAD
+void *tutor()
+{
+
+}
+
+
+
+
+//  MAIN PROGRAM
 int main(int argc, char *argv[])
 {
- long i;
- //Initializes threads
- pthread_t *students;
- pthread_t *tutors;
- pthread_t coordinator;
- //Casts arguments into integer and returns error if no digits are passed
- sscanf(argv[1],"%d",&numOfStudents);
- sscanf(argv[2], "%d", &numOfTutors);
- sscanf(argv[3], "%d", &numOfChairs);
- sscanf(argv[4], "%d", &help);
- //Allocates dynamically threads for students and tutors
- students = malloc(sizeof(pthread_t) * numOfStudents);
- tutors = malloc(sizeof(pthread_t) * numOfStudents);
- //Allocates dynamically student queue
- studentIDQueue = malloc(1000 * sizeof(int*));
- priorities = malloc(1000 * sizeof(int*));
- newStudentInQueue = 0;
- //Initialize semaphores
- sem_init(&waitingRoom, 0, numOfChairs);
- printf("hello");
- //sem_init(&studentQueue, 0, 0);
- //sem_init(&tutorQueue, 0, 0);
- //sem_init(&requiredNumberOfHelpTimes, 0, help);
- //Creates student threads
- for(i = 0; i < numOfStudents; i++)
- {
-    assert(pthread_create(&students[i], NULL, student, (void *) i) == 0);
- }
- /*
- //Creates tutor threads
- for(i = 0; i < numOfTutors; i++) 
- {
-     assert(pthread_create(&tutors[i], NULL, thread, (void *) i) == 0);
- }
- */
- pthread_create(&coordinator, NULL, thread, NULL);
- };
+    //  THREADS
+    pthread_t * students;
+    pthread_t * tutors;
+    pthread_t coordinator;
+
+    //  LOCALS
+    int numberOfStudents;
+    int numberOfTutors;
+    int numberOfChairs;
+    int numberOfHelp;
+    long i;
+
+    //Casts arguments into integer and returns error if no digits are passed
+    sscanf(argv[1], "%d", &numberOfStudents);
+    sscanf(argv[2], "%d", &numberOfTutors);
+    sscanf(argv[3], "%d", &numberOfChairs);
+    sscanf(argv[4], "%d", &numberOfHelp);
+
+    //  ALLOCATE THREADS
+    students = malloc(sizeof(pthread_t) * numberOfStudents);
+    tutors = malloc(sizeof(pthread_t) * numberOfTutors);
+
+    //  ALLOCATE DATA
+    studentIDQueue = malloc(1000 * sizeof(int*));
+    priorities = malloc(1000 * sizeof(int*));
+    newStudentInQueue = 0;
+
+    //  INITIALIZE SEMAPHORES
+    sem_init(&mutexChairs, 0, 1);
+    sem_init(&mutexArrivedStudentQ, 0, 1);
+    sem_init(&mutexWaitingStudentQ, 0, 1);
+    sem_init(&coordinatorWaiting, 0, 0);
+    sem_init(&tutorWaiting, 0, 0);
+
+
+    //Creates student threads
+    for(i = 0; i < numberOfStudents; i++)
+    {
+        //  returns thread id
+        assert(pthread_create(&students[i], NULL, student, (void *) i) == 0);
+    }
+
+    /*
+    //Creates tutor threads
+    for(i = 0; i < numOfTutors; i++) 
+    {
+        assert(pthread_create(&tutors[i], NULL, thread, (void *) i) == 0);
+    }
+    */
+}
